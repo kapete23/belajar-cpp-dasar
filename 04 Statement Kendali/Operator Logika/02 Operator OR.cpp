bc4960750b5bff882b79fdf563f5cccfd72f6050
kapete23 <kevingokils97@gmail.com>
2021-09-27 23:47:00 +0700

    Menambahkan file <02 Operator OR.cpp> pada folder operator logika

---
+++ 04 Statement Kendali/Operator Logika/02 Operator OR.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <conio.h>
+#define bersihkan_layar system("cls")
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bersihkan_layar;
+    double income;
+    int years;
+
+    cout << "Apakah ini masukan tahunan Anda? ";
+    cin >> income;
+    cout << "berapa banyak anda bekerja setiap tahunnya "
+    << "Pekerjaan anda sekarang? ";
+    cin >> years;
+
+    //Statement kendali IF ELSE yang menggunakan operator logika (operator binary) "||"
+    //Operator || dibaca OR (atau)
+    if (income >= 35000 || years > 5)
+        cout << "\nKwalifikasi Anda.\n";
+    else {
+        cout << "\nAnda akan mendapatkan gaji paling tidak $35,000 atau lebih\n";
+        cout << "setelah bekerja lebih dari lima tahun.\n";
+    }
+
+    getch();
+    bersihkan_layar;
+    return 0;
+}
