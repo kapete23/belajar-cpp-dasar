a780f0301bb0c204b3bbca8194eb7dee36dbae32
kapete23 <kevingokils97@gmail.com>
2021-09-30 23:53:10 +0700

    Menambah file <03 print full pyramid> pada folder <05/membuat pola atau pattern>

---
+++ 05 Struktur Perulangan/Membuat Pola atau Pattern/03 print full pyramid.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <conio.h>
+#define bersihkan_layar system("cls");
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bersihkan_layar;
+
+    // 01 print half pyramid menggunakan karakter bintang
+    // cout << " * \n";
+    // cout << " ** \n";
+    // cout << " *** \n";
+    // cout << " **** \n";
+    // cout << " ***** \n";
+
+    // 02 print rotasi 180 derajat half pyramid menggunakan karakter bintang
+    // cout << "     * \n";
+    // cout << "    ** \n";
+    // cout << "   *** \n";
+    // cout << "  **** \n";
+    // cout << " ***** \n";
+
+    // 03 print inverted half pyramid menggunakan karakter bintang
+    // cout << " ***** \n";
+    // cout << " **** \n";
+    // cout << " *** \n";
+    // cout << " ** \n";
+    // cout << " * \n";
+
+    // 04 print rotasi 360 half pyramid menggunakan karakter bintang
+    // cout << " ***** \n";
+    // cout << "  **** \n";
+    // cout << "   *** \n";
+    // cout << "    ** \n";
+    // cout << "     * \n";
+
+    getch();
+    bersihkan_layar;
+    return 0;
+}
