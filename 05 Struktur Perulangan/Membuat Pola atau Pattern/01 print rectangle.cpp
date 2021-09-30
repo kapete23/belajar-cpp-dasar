bd6a2c878cdabbddc928be47b813543bc9a7cc6d
kapete23 <kevingokils97@gmail.com>
2021-09-30 23:51:22 +0700

    Menambah file <01 print rectangle.cpp> pada folder <05/membuat pola atau pattern>

---
+++ 05 Struktur Perulangan/Membuat Pola atau Pattern/01 print rectangle.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <conio.h>
+#define bersihkan_layar system("cls");
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bersihkan_layar;
+
+    // 01 print solid rectangle menggunakan karakter bintang 12 : 20 = l : P
+    // cout << " ******************** \n";
+    // cout << " ******************** \n";
+    // cout << " ******************** \n";
+    // cout << " ******************** \n";
+    // cout << " ******************** \n";
+    // cout << " ******************** \n";
+    // cout << " ******************** \n";
+    // cout << " ******************** \n";
+    // cout << " ******************** \n";
+    // cout << " ******************** \n";
+    // cout << " ******************** \n";
+    // cout << " ******************** \n";
+
+    // 02 print hollow rectangle menggunakan karakter bintang 12 : 20 = l : P
+    // cout << "     * \n";
+    // cout << "    ** \n";
+    // cout << "   *** \n";
+    // cout << "  **** \n";
+    // cout << " ***** \n";
+
+    getch();
+    bersihkan_layar;
+    return 0;
+}
