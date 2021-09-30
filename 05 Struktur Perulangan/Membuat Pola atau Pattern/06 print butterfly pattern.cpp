8ed9a6dca17ca30d6f5c97133058cf7862956dee
kapete23 <kevingokils97@gmail.com>
2021-09-30 23:55:14 +0700

    Menambah file <06 print butterfly pattern.cpp> pada folder <05/membuat pola atau pattern>

---
+++ 05 Struktur Perulangan/Membuat Pola atau Pattern/06 print butterfly pattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+#define bersihkan_layar system("cls");
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bersihkan_layar;
+
+    //Keluaran pola...
+    // cout << "A\n";
+    // cout << "AB\n";
+    // cout << "ABC\n";
+    // cout << "ABCD\n";
+    // cout << "ABCDE\n";
+
+    getch();
+    bersihkan_layar;
+    return 0;
+}
