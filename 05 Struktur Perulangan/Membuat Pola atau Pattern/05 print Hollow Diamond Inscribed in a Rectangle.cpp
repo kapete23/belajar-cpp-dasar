5a70fc2c31d3b52980a1fc2453ef167a9bef69c7
kapete23 <kevingokils97@gmail.com>
2021-09-30 23:54:42 +0700

    Menambah file <05 print Hollow Diamond Inscribed in a Rectangle.cpp> pada folder <05/membuat pola atau pattern>

---
+++ 05 Struktur Perulangan/Membuat Pola atau Pattern/05 print Hollow Diamond Inscribed in a Rectangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <conio.h>
+#define bersihkan_layar system("cls");
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bersihkan_layar;
+
+    //Keluaran pola...
+    // cout << "1\n";
+    // cout << "12\n";
+    // cout << "123\n";
+    // cout << "1234\n";
+    // cout << "12345\n";
+
+    getch();
+    bersihkan_layar;
+    return 0;
+}
